ec0e6c2871e293e69e9f438298dea3819072851d
Sylvio Lima <dmsylvio@gmail.com>
2026-02-24 13:03:53 -0500

    refactor(marketing): simplify layout component by removing unnecessary padding

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="min-h-dvh p-6">
+    <main className="min-h-dvh">
       <div className="w-full">{children}</div>
     </main>
   );
